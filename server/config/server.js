53c43f16d08cb450746dd872a66f996c38e4f201
Julien Bisconti <julienbi@unity3d.com>
2020-08-27 22:11:47 -0300

    feat: start api

---
+++ server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c2d198239ab672fbf6cff72570b7f28e'),
+    },
+  },
+});
---
+++ server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8d267a68-edf0-486c-998f-a9d0abdc73fd'
+};
