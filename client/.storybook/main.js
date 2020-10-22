d65da29aeb74fcd5826b3bde494e6409c83a0c6d
Sergio Eduardo Flores Jr <sergio.flores.junior17@gmail.com>
2020-10-22 20:35:45 -0300

    add path mapping to storybook config

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -1,4 +1,10 @@
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
   addons: ['@storybook/addon-essentials'],
+  webpackFinal: config => {
+    // ? Load files based on src
+    config.resolve.modules.push(`${process.cwd()}/src`);
+
+    return config;
+  },
 };
