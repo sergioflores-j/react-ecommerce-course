dfb2b4fa586b256a43b1f60c452ea52aeed16217
Sergio Eduardo Flores Jr <sergio.flores.junior17@gmail.com>
2021-02-16 21:32:30 -0300

    feat: add banner

---
+++ client/src/components/Banner/test.tsx
@@ -0,0 +1,29 @@
+import { screen } from '@testing-library/react';
+import { renderWithTheme } from 'utils/tests/helpers';
+
+import Banner from '.';
+
+const props = {
+  img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+  title: 'Defy death',
+  subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+  buttonLabel: 'Buy now',
+  buttonLink: '/games/defy-death',
+};
+
+describe('<Banner />', () => {
+  it('should render correctly', () => {
+    const { container } = renderWithTheme(<Banner {...props} />);
+
+    expect(
+      screen.getByRole('heading', { name: /Defy Death/i })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('heading', { name: /Play the new /i })
+    ).toBeInTheDocument();
+    expect(screen.getByRole('link')).toBeInTheDocument();
+    expect(screen.getByRole('img')).toBeInTheDocument();
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
