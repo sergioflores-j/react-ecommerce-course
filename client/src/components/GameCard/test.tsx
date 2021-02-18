c83207ada9ad3afefe92f5fbeb40be2977bd4aed
Sergio Eduardo Flores Jr <sergio.flores.junior17@gmail.com>
2021-02-17 21:17:40 -0300

    feat: add gameCard

---
+++ client/src/components/GameCard/test.tsx
@@ -0,0 +1,72 @@
+import { fireEvent, screen } from '@testing-library/react';
+import theme from 'styles/theme';
+import { renderWithTheme } from 'utils/tests/helpers';
+
+import GameCard from '.';
+
+const props = {
+  title: 'Population Zero',
+  developer: 'Rockstar Games',
+  img: 'https://source.unsplash.com/user/willianjusten/300x140',
+  price: 'R$ 235,00',
+};
+
+describe('<GameCard />', () => {
+  it('should render correctly', () => {
+    renderWithTheme(<GameCard {...props} />);
+
+    expect(
+      screen.getByRole('heading', { name: props.title })
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('heading', { name: props.developer })
+    ).toBeInTheDocument();
+
+    expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
+      'src',
+      props.img
+    );
+
+    expect(screen.getByLabelText(/add to wishlist/i)).toBeInTheDocument();
+  });
+
+  it('should render price in label', () => {
+    renderWithTheme(<GameCard {...props} />);
+    const price = screen.getByText(props.price);
+
+    expect(price).not.toHaveStyleRule('text-decoration', 'line-through');
+    expect(price).toHaveStyleRule('background-color', theme.colors.secondary);
+  });
+
+  it('should render a line-through in price when promotional', () => {
+    renderWithTheme(<GameCard {...props} promotionalPrice="R$ 15,00" />);
+
+    const price = screen.getByText(props.price);
+    const promPrice = screen.getByText('R$ 15,00');
+
+    expect(price).toHaveStyleRule('text-decoration', 'line-through');
+
+    expect(promPrice).not.toHaveStyleRule('text-decoration', 'line-through');
+    expect(promPrice).toHaveStyleRule(
+      'background-color',
+      theme.colors.secondary
+    );
+  });
+
+  it('should render a filled favorite icon when favorite is true', () => {
+    renderWithTheme(<GameCard {...props} favorite />);
+
+    expect(screen.getByLabelText(/remove from wishlist/i)).toBeInTheDocument();
+  });
+
+  it('should call onFav method when favorite is clicked', () => {
+    const onFav = jest.fn();
+
+    renderWithTheme(<GameCard {...props} onFav={onFav} />);
+
+    fireEvent.click(screen.getAllByRole('button')[0]);
+
+    expect(onFav).toBeCalled();
+  });
+});
