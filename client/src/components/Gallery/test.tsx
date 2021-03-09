bc43e67b53d53abfb10390a05ac5f5688da4d74f
Sergio Eduardo Flores Jr <sergio.flores.junior17@gmail.com>
2021-03-08 21:32:02 -0300

    feat: add gallery modal slider

--- client/src/components/Gallery/test.tsx
+++ client/src/components/Gallery/test.tsx
@@ -1,5 +1,5 @@
 import 'match-media-mock';
-import { screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import { renderWithTheme } from 'utils/tests/helpers';
 
 import Gallery from '.';
@@ -16,5 +16,65 @@ describe('<Gallery />', () => {
     expect(
       screen.getByRole('button', { name: /thumb - gallery image 2/i })
     ).toHaveAttribute('src', mockItems[1].src);
+  });
+
+  it('should handle open modal', () => {
+    renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />);
+
+    const modal = screen.getByLabelText('modal');
+
+    // verificar se o modal tá escondido
+    expect(modal.getAttribute('aria-hidden')).toBe('true');
+    expect(modal).toHaveStyle({ opacity: 0, pointerEvents: 'none' });
+
+    // clicar no botão de abrir o modal e verificar se ele abriu
+    fireEvent.click(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 1/i })
+    );
+    expect(modal.getAttribute('aria-hidden')).toBe('false');
+    expect(modal).toHaveStyle({ opacity: 1 });
+  });
+
+  it('should handle open modal with selected image', async () => {
+    renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />);
+
+    fireEvent.click(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 2/i })
+    );
+
+    const img = await screen.findByRole('img', { name: /gallery image 2/i });
+
+    expect(img.parentElement?.parentElement).toHaveClass('slick-active');
+  });
+
+  it('should handle close modal when overlay of button clicked', () => {
+    renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />);
+
+    const modal = screen.getByLabelText('modal');
+
+    fireEvent.click(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 1/i })
+    );
+
+    fireEvent.click(screen.getByRole('button', { name: /close modal/i }));
+    expect(modal.getAttribute('aria-hidden')).toBe('true');
+    expect(modal).toHaveStyle({ opacity: 0 });
+  });
+
+  it('should handle close modal when ESC button is pressed', () => {
+    const { container } = renderWithTheme(
+      <Gallery items={mockItems.slice(0, 2)} />
+    );
+
+    const modal = screen.getByLabelText('modal');
+
+    fireEvent.click(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 1/i })
+    );
+
+    fireEvent.keyUp(container, { key: 'Escape' });
+
+    expect(modal.getAttribute('aria-hidden')).toBe('true');
+    expect(modal).toHaveStyle({ opacity: 0 });
   });
 });
