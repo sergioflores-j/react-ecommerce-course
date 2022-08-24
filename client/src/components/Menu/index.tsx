739dfb9956689c00dfac243c122ab77668ffb818
Flores Junior, S.E. (Sergio) <sergio.flores@nn.nl>
2022-08-24 20:51:15 +0200

    feat: menu links

--- client/src/components/Menu/index.tsx
+++ client/src/components/Menu/index.tsx
@@ -1,20 +1,26 @@
 import { useState } from 'react';
+import Link from 'next/link';
 import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2';
-import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outlined/ShoppingCart';
 import { Search as SearchIcon } from '@styled-icons/material-outlined/Search';
 import { Close as CloseIcon } from '@styled-icons/material-outlined/Close';
-import Link from 'next/link';
 
 import Logo from 'components/Logo';
-import * as S from './styles';
 import Button from 'components/Button';
 import MediaMatch from 'components/MediaMatch';
+import { GameItemProps } from 'components/GameItem';
+import CartDropdown from 'components/CartDropdown';
+import CartIcon from 'components/CartIcon';
+import UserDropdown from 'components/UserDropdown';
+
+import * as S from './styles';
 
 export type MenuProps = {
   username?: string;
+  items?: GameItemProps[];
+  total?: string;
 };
 
-const Menu = ({ username }: MenuProps) => {
+const Menu = ({ username, items, total }: MenuProps) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
@@ -38,7 +44,9 @@ const Menu = ({ username }: MenuProps) => {
           <Link href="/" passHref>
             <S.MenuLink>Home</S.MenuLink>
           </Link>
-          <S.MenuLink href="#">Explore</S.MenuLink>
+          <Link href="/games" passHref>
+            <S.MenuLink>Explore</S.MenuLink>
+          </Link>
         </S.MenuNav>
       </MediaMatch>
 
@@ -47,16 +55,28 @@ const Menu = ({ username }: MenuProps) => {
           <SearchIcon aria-label="Search" />
         </S.IconWrapper>
         <S.IconWrapper>
-          <ShoppingCartIcon aria-label="Open Shopping Cart" />
+          <MediaMatch greaterThan="medium">
+            <CartDropdown items={items} total={total} />
+          </MediaMatch>
+
+          <MediaMatch lessThan="medium">
+            <Link href="/cart">
+              <a>
+                <CartIcon quantity={items?.length} />
+              </a>
+            </Link>
+          </MediaMatch>
         </S.IconWrapper>
 
-        {!username && (
-          <MediaMatch greaterThan="medium">
+        <MediaMatch greaterThan="medium">
+          {!username ? (
             <Link href="/sign-in" passHref>
               <Button as="a">Sign in</Button>
             </Link>
-          </MediaMatch>
-        )}
+          ) : (
+            <UserDropdown username={username} />
+          )}
+        </MediaMatch>
       </S.MenuGroup>
 
       <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
@@ -66,12 +86,19 @@ const Menu = ({ username }: MenuProps) => {
           <Link href="/" passHref>
             <S.MenuLink>Home</S.MenuLink>
           </Link>
-          <S.MenuLink href="#">Explore</S.MenuLink>
+
+          <Link href="/games" passHref>
+            <S.MenuLink>Explore</S.MenuLink>
+          </Link>
 
           {!!username && (
             <>
-              <S.MenuLink href="#">My account</S.MenuLink>
-              <S.MenuLink href="#">Wishlist</S.MenuLink>
+              <Link href="/profile/me" passHref>
+                <S.MenuLink>My profile</S.MenuLink>
+              </Link>
+              <Link href="/wishlist" passHref>
+                <S.MenuLink>Wishlist</S.MenuLink>
+              </Link>
             </>
           )}
         </S.MenuNav>
